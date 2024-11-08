2061b343e84699d21eb306bc7a23582f40b7915d
Melissa Buckingham <m19b88@gmail.com>
2024-11-08 21:16:41 +0000

    added search functionality

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
 def all_listings(request):
     """ A view to show all listings, including sorting and search queries """
     listings = Product.objects.all()
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('listings'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            listings = listings.filter(queries)
+
     context = {
         'listings': listings,
+        'search_term': query,
     }
     return render(request, 'listings/listings.html', context)
 
