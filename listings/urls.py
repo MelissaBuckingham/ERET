5295ca82bb0106a439b88ce516d883d5daadfb95
Melissa Buckingham <m19b88@gmail.com>
2024-11-08 20:40:55 +0000

    added product details functionality

--- listings/urls.py
+++ listings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.all_listings, name='listings'),
-    path('<listing_id>', views.listing_detail, name='listing_detail'),
+    path('<product_id>', views.listing_detail, name='listing_detail'),
 ]
