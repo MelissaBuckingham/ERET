398beeb9f6f87f975b694d846448f2baf184dbb8
Melissa Buckingham <m19b88@gmail.com>
2024-11-08 17:33:36 +0000

    added listing urls etc

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def all_listings(request):
+    """ A view to show all listings, including sorting and search queries """
+    listings = Product.objects.all()
+    context = {
+        'listings': listings,
+    }
+    return render(request, 'listings/listings.html', context)
